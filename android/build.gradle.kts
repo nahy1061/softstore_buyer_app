fcc756bac69029c859c391855119d03febd3d06f
Naheed <naheed.official99@gmail.com>
2026-08-18 13:56:52 +0500

    fix(android): remove faulty shared_preferences_android dependency override and clean Gradle setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-}
-
 allprojects {
     repositories {
         google()
